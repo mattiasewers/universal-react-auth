50e28887d24526f3462a95cd96325ac589bfab79
mattiasewers <mattias.ewers@gmail.com>
2018-02-20 21:17:46 +0100

    setup jwt auth

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import { render } from '@jaredpalmer/after';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
+import jwt from 'express-jwt';
 import schema from './graphql/schema';
 
 import routes from '../shared/routes';
+
+require('dotenv').config();
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
@@ -12,7 +15,12 @@ const server = express();
 server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
+  .use(
+    '/graphql',
+    jwt({ secret: process.env.RAZZLE_SECRET, credentialsRequired: false }),
+    bodyParser.json(),
+    graphqlExpress(req => ({ schema, context: { user: req.user } }))
+  )
   .use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
   .get('/*', async (req, res) => {
     try {
